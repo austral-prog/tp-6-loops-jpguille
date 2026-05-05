167c11e68df80e64f6eec034fa4ed6e8d4670bcb
jpguille <jpguillen2008@gmail.com>
2026-05-04 22:24:56 -0300

    Update while_basics.py

--- while_basics.py
+++ while_basics.py
@@ -1,25 +1,18 @@
-# Replace the "ANSWER HERE" for your answer
-
 def countdown(n):
-    """
-    Retorna una lista con la cuenta regresiva desde n hasta 0.
-    Si n < 0, retorna una lista vacia.
-
-    Ejemplo: countdown(5) -> [5, 4, 3, 2, 1, 0]
-    Ejemplo: countdown(0) -> [0]
-    Ejemplo: countdown(-1) -> []
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+    new = []
+    while n >= 0:
+        new.append(n)
+        n = n - 1
+    return new
 
 
 def double_until(limit):
-    """
-    Comenzando desde 1, va duplicando el valor y agrega cada uno
-    a una lista mientras sea menor o igual a limit.
-    Si limit < 1, retorna una lista vacia.
 
-    Ejemplo: double_until(10) -> [1, 2, 4, 8]
-    Ejemplo: double_until(1) -> [1]
-    Ejemplo: double_until(0) -> []
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+    i = 1
+    lst = [i]
+    if limit < 1:
+        return []
+    while i*2 <= limit:
+         i *= 2
+         lst.append(i)
+    return lst
