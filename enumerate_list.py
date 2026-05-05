887da3044e211f4bfc1b8f5733056ce90f6a0562
jpguille <jpguillen2008@gmail.com>
2026-05-04 22:21:16 -0300

    Update enumerate_list.py

--- enumerate_list.py
+++ enumerate_list.py
@@ -1,22 +1,18 @@
-# Replace the "ANSWER HERE" for your answer
-
 def enumerate_list(lst):
-    """
-    Dada una lista de strings, retorna una nueva lista donde cada elemento
-    tiene el formato "indice. valor". Los strings vacios se deben saltear
-    y no deben aparecer en la lista resultante.
-    El indice debe ser consecutivo (no el indice original).
-
-    Ejemplo: enumerate_list(["Red", "Green", "", "White"]) -> ["0. Red", "1. Green", "2. White"]
-    """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    index = -1
+    new = []
+    for element in lst:
+        if element != "":
+            index += 1
+            new.append(f"{index}. {element}")
+    return new
 
 def enumerate_backwards(lst):
-    """
-    Igual que enumerate_list, pero cada palabra debe estar escrita al reves.
-    Los strings vacios se deben saltear.
 
-    Ejemplo: enumerate_backwards(["Red", "Green", ""]) -> ["0. deR", "1. neerG"]
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+    index = -1
+    new = []
+    for element in lst:
+        if element != "":
+            index += 1
+            new.append(f"{index}. {element[::-1]}")
+    return new
