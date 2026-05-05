09cb4c1bf4150b47e174590f24b16b42d903a57b
jpguille <jpguillen2008@gmail.com>
2026-05-04 22:23:39 -0300

    Update powers.py

--- powers.py
+++ powers.py
@@ -1,20 +1,10 @@
-# Replace the "ANSWER HERE" for your answer
-
 def power(base, exp):
-    """
-    Retorna base elevado a exp usando un bucle for.
-    exp es siempre >= 0.
 
-    Ejemplo: power(2, 3) -> 8  (2*2*2)
-    """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if exp >= 0:
+        return (base ** exp)
 
 def sum_of_powers(base, max_exp):
-    """
-    Retorna la suma de base^0 + base^1 + ... + base^max_exp.
-    Debe USAR la funcion power.
-
-    Ejemplo: sum_of_powers(2, 3) -> 15  (1+2+4+8)
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+    suma = 0
+    for exp in range(0, max_exp+1):
+        suma += power(base, exp)
+    return suma
